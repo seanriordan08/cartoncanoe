d5d5bf821c5811979f4ee47d91de85585813a0e0
Sean Riordan <sean.riordan08@gmail.com>
2013-05-06 09:39:44 -0600

    D2 - completed

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,7 +9,7 @@ private
 #Ensure that there are no line items referencing this product
 def ensure_not_referenced_by_any_line_item
 	if line_items.empty?
-	return true
+		return true
 	else
 		errors.add(:base, 'Line Items present')
 		return false
