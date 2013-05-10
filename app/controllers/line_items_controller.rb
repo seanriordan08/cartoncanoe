213da35d66f522471ea23000d6b8601f3df27383
Sean Riordan <sean.riordan08@gmail.com>
2013-05-10 14:47:45 -0600

    E1

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -39,19 +39,22 @@ class LineItemsController < ApplicationController
 
   # POST /line_items
   # POST /line_items.json
-  def create
-    @line_item = LineItem.new(params[:line_item])
+def create
+ @cart = current_cart
+ product = Product.find(params[:product_id])
+ @line_item = @cart.line_items.build
+ @line_item.product = product
 
-    respond_to do |format|
-      if @line_item.save
-        format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
-        format.json { render json: @line_item, status: :created, location: @line_item }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
-      end
-    end
+ respond_to do |format|
+ if @line_item.save
+  format.html { redirect_to(@line_item.cart, notice:'Line item was successfully created.') }
+  format.json { render json: @line_item, status: :created, :location => @line_item }
+  else
+   format.html { render :action => "new" }
+   format.json { render json: @line_item.errors, status: :unprocessable_entity }
   end
+ end
+end
 
   # PUT /line_items/1
   # PUT /line_items/1.json
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,13 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
+
+	if session[:counter].nil?
+		@visits = 1
+	else
+		visits = session[:visit]
+		@visits = visits + 1
+	end
   end
   
 end
