93925b0e4e857fdeb27e40717d8da5e6f480d6a4
Sean Riordan <sean.riordan08@gmail.com>
2013-05-31 13:00:43 -0600

    Iteration - H1 - Auto Email Alert Works

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,17 @@ Depot::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  # Configures Email Notification Settings
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+	:address			=> "smtp.gmail.com",
+	:port				=> 587,
+	:domain				=> "domain.of.sender.net",
+	:authentication 	=> "plain",
+	:user_name			=> "sean.riordan08@gmail.com",
+	:password			=> "harmanka",
+	:enable_starttls_auto	=> true
+	}
 end
