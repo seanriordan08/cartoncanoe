159038d43a51397ef73a4868ff7eb167fc12abfe
Sean Riordan <sean.riordan08@gmail.com>
2013-06-20 10:49:30 -0600

    Commit A1

--- app/models/product.rb
+++ app/models/product.rb
@@ -22,5 +22,5 @@ end
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
   validates :image_url, :format => {:with => %r{\.(gif|jpg|png)$}i,
-            :message => 'must be a URL for GIF, JPG or PNG image.'}
+            :message => 'must be a URL for GIF, JPG or PNG image. (images/example.png)'}
 end
