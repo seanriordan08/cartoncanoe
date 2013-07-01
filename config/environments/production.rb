102dfb61ba75b66bea2ec3171c99130d69363cb6
Sean Riordan <sean.riordan08@gmail.com>
2013-07-01 14:04:05 -0600

    more

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ Depot::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # config.assets.compile = false (old)
+  config.assets.precompile += %w( application.css )
 
   # Generate digests for assets URLs
   config.assets.digest = true
