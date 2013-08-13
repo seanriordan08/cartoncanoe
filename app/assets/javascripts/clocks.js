8bf24ddaecbedb5e687c01dc538ca1325d3c3a66
Sean Riordan <sean.riordan08@gmail.com>
2013-08-13 16:54:31 -0600

    data check before test migration

--- app/assets/javascripts/clocks.js
+++ app/assets/javascripts/clocks.js
@@ -1,21 +1,42 @@
 ﻿function updateClock()
-     {
-     var currentTime = new Date ();
-     var currentHours = currentTime.getHours ();
-     var currentMinutes = currentTime.getMinutes ();
-     var currentSeconds = currentTime.getSeconds ();
+{
+	$("#counterNeedle").hide();
+
+	var currentTime = new Date ();
+	var currentHours = currentTime.getHours ();
+	var currentMinutes = currentTime.getMinutes ();
+	var currentSeconds = currentTime.getSeconds ();
 	 
 	 //Calculate Hours Postition
 	var currentHoursPosition = 17;
-	if ( 0 < currentHours < 13){
-		if (currentHours <= 6 ){ currentHoursPosition = 17;}
-		else if (currentHours == 7 ){ currentHoursPosition = 15; }
-		else if (currentHours == 8 ){ currentHoursPosition = 30; }
-		else if (currentHours == 9 ){ currentHoursPosition = 65; }
-		else if (currentHours == 10 ){ currentHoursPosition = 98; }
-		else if (currentHours == 11 ){ currentHoursPosition = 132; }
-		else if (currentHours == 12 ){ currentHoursPosition = 166; }
-		else { currentHoursPosition = 17; }
+	if ( currentHours > 0 && currentHours < 13){
+		if (currentHours < 7 ){ 
+			currentHoursPosition = 17; 
+			$("#counterBG1").css( "color", "#990000");
+		}
+		else if (currentHours == 7 ){ 
+			currentHoursPosition = 15; 
+		}
+		else if (currentHours == 8 ){ 
+			currentHoursPosition = 30;
+			$("#counterBG1").css( "color", "#990000");			
+		}
+		else if (currentHours == 9 ){ 
+			currentHoursPosition = 65; 
+			$("#counterBG1").css( "color", "#990000"); 
+		}
+		else if (currentHours == 10 ){ 
+			currentHoursPosition = 98; 
+		}
+		else if (currentHours == 11 ){ 
+			currentHoursPosition = 132;
+		}
+		else if (currentHours == 12 ){ 
+			currentHoursPosition = 166; 
+		}
+		else { currentHoursPosition = 17; 
+			$("#counterBG1").css( "color", "#990000"); 
+		}
 	}
 	else { 
 		currentHoursPosition = 166;
@@ -35,9 +56,11 @@
 	 
      $("#counterNeedle").css(  "top",  position);
          
-  }
+}
 
  $(document).ready(function()
 {
+    $("#counterNeedle").hide();
     setInterval('updateClock()', 1000);
+	$("#counterNeedle").show();
 });
