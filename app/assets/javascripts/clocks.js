3994ffd0f01356eec3425f932e690e46847d8690
Sean Riordan <sean.riordan08@gmail.com>
2013-08-14 19:55:09 -0600

    Cleaned incorrect login credential notices Added User Logout notices Added product editing pagination

--- app/assets/javascripts/clocks.js
+++ app/assets/javascripts/clocks.js
@@ -1,66 +1,65 @@
-﻿function updateClock()
-{
-	$("#counterNeedle").hide();
+﻿function updateClock(){
 
-	var currentTime = new Date ();
-	var currentHours = currentTime.getHours ();
-	var currentMinutes = currentTime.getMinutes ();
-	var currentSeconds = currentTime.getSeconds ();
-	 
-	 //Calculate Hours Postition
-	var currentHoursPosition = 17;
-	if ( currentHours > 0 && currentHours < 13){
-		if (currentHours < 7 ){ 
-			currentHoursPosition = 17; 
-			$("#counterBG1").css( "color", "#990000");
-		}
-		else if (currentHours == 7 ){ 
-			currentHoursPosition = 15; 
-		}
-		else if (currentHours == 8 ){ 
-			currentHoursPosition = 30;
-			$("#counterBG1").css( "color", "#990000");			
-		}
-		else if (currentHours == 9 ){ 
-			currentHoursPosition = 65; 
-			$("#counterBG1").css( "color", "#990000"); 
-		}
-		else if (currentHours == 10 ){ 
-			currentHoursPosition = 98; 
-		}
-		else if (currentHours == 11 ){ 
-			currentHoursPosition = 132;
-		}
-		else if (currentHours == 12 ){ 
-			currentHoursPosition = 166; 
-		}
-		else { currentHoursPosition = 17; 
-			$("#counterBG1").css( "color", "#990000"); 
-		}
+ $("#counterNeedle").hide();
+
+ var currentTime = new Date ();
+ var currentHours = currentTime.getHours ();
+ var currentMinutes = currentTime.getMinutes ();
+ var currentSeconds = currentTime.getSeconds ();
+ 
+ //Calculate Hours Postition
+ var currentHoursPosition = 17;
+ if ( currentHours > 0 && currentHours < 13){
+	if (currentHours < 7 ){ 
+		currentHoursPosition = 17; 
+		$("#counterBG1").css( "color", "#990000");
 	}
-	else { 
-		currentHoursPosition = 166;
-		$("#counterBG1").css( "color", "#666666");
+	else if (currentHours == 7 ){ 
+		currentHoursPosition = 15; 
 	}
-	 
-	 /*Calculate Minutes Postition
-	var currentMinutesPosition = Math.round(currentMinutes / 2);
-	if ( 0 < currentHours < 7 ){ currentMinutesPosition = 0; }
-	else if (currentHours < 7){ currentMinutesPosition = currentMinutesPosition; }
-	else if (currentHours > 11){ currentMinutesPosition = 0; }  */
-	 
-	 var updatedValue = 0;
-	 var position = 0;
-	 var updatedValue = currentHoursPosition;
-	 var position = updatedValue + "px";
-	 
-     $("#counterNeedle").css(  "top",  position);
-         
+	else if (currentHours == 8 ){ 
+		currentHoursPosition = 30;
+		$("#counterBG1").css( "color", "#990000");			
+	}
+	else if (currentHours == 9 ){ 
+		currentHoursPosition = 65; 
+		$("#counterBG1").css( "color", "#990000"); 
+	}
+	else if (currentHours == 10 ){ 
+		currentHoursPosition = 98; 
+	}
+	else if (currentHours == 11 ){ 
+		currentHoursPosition = 132;
+	}
+	else if (currentHours == 12 ){ 
+		currentHoursPosition = 166; 
+	}
+	else { currentHoursPosition = 17; 
+		$("#counterBG1").css( "color", "#990000"); 
+	}
+ }
+ else { 
+	currentHoursPosition = 166;
+	$("#counterBG1").css( "color", "#666666");
+ }
+ 
+ /*Calculate Minutes Postition
+ var currentMinutesPosition = Math.round(currentMinutes / 2);
+ if ( 0 < currentHours < 7 ){ currentMinutesPosition = 0; }
+ else if (currentHours < 7){ currentMinutesPosition = currentMinutesPosition; }
+ else if (currentHours > 11){ currentMinutesPosition = 0; }  */
+ 
+ var updatedValue = 0;
+ var position = 0;
+ var updatedValue = currentHoursPosition;
+ var position = updatedValue + "px";
+
+ $("#counterNeedle").css(  "top",  position);	 
 }
 
- $(document).ready(function()
+$(document).ready(function()
 {
-    $("#counterNeedle").hide();
-    setInterval('updateClock()', 1000);
-	$("#counterNeedle").show();
+ $("#counterNeedle").hide();
+ setInterval('updateClock()', 1000);
+ $("#counterNeedle").show();
 });
