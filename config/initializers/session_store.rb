3ba119b6a45b9802b70e911f17b54a1bd97d5b88
Sean Riordan <sean.riordan08@gmail.com>
2013-09-17 11:52:19 -0600

    session_store restored

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Depot::Application.config.session_store :cookie_store, 
-	key: '_depot_session',
-	expire_after: '30.minutes'
+Depot::Application.config.session_store :cookie_store, key: '_depot_session'
+
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
