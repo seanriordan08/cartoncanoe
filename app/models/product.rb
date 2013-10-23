5221365aed7fcb8bdb6268637d9691ee9a89d974
Sean Riordan <sean.riordan08@gmail.com>
2013-10-23 11:56:19 -0600

    rollback last commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,7 +10,7 @@
 	validates :title, :description, :image, :unit_of_measure, :presence => true
 	validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
 	validates :portion_size, :numericality => {:greater_than_or_equal_to => 0.01}
-	#validates :title, :uniqueness => true
+	validates :title, :uniqueness => true
 
 	validates :image, :format => {:with => %r{\.(gif|jpg|png)$}i,
 			:message => 'must be a URL for GIF, JPG or PNG image. (images/example.jpg)'}
