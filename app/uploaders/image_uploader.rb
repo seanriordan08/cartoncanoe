8943fd79a23172aa4f464f445ee8ba777ec48f22
Sean Riordan <sean.riordan08@gmail.com>
2013-10-23 17:26:12 -0600

    Uploader modified to include common image dir Uploader modified to specify noImage2 file as default

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,13 +13,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "public/assets/images"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url 
-    # "public/assets/images/noImage2.jpg" #+ [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url 
+    "public/assets/images/noImage2.jpg" #+ [version_name, "default.png"].compact.join('_')
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
