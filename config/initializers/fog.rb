30893939afed2f61d957134c217b440411514182
Sean Riordan <sean.riordan08@gmail.com>
2013-11-01 16:12:25 -0600

    "ENV[]" wrappers removed

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -5,13 +5,13 @@ CarrierWave.configure do |config|
   #Configuration for Amazon S3
   
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV['AKIAJF2G3XHMDSIW22KQ'],       # required
-    :aws_secret_access_key  => ENV['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'],                        # required
+    :aws_access_key_id      => 'AKIAJF2G3XHMDSIW22KQ',       # required
+    :aws_secret_access_key  => '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',                        # required
     #:region                 => ENV['us-west-1'],                 # optional, defaults to 'us-east-1'
     #:host                   => 's3.amazonaws.com',           # optional, defaults to nil
     #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
   }
-  config.fog_directory  = ENV['cartoncanoeproducts']                     # required
+  config.fog_directory  = 'cartoncanoeproducts'                     # required
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 
