f443374a52fb2bd442152614f60dc5f218ac5a3f
Sean Riordan <sean.riordan08@gmail.com>
2013-11-07 09:18:30 -0700

    Landing Page Overhaul + Re-Design (Rough Draft)

---
+++ app/assets/javascripts/landingPage.js
@@ -0,0 +1,29 @@
+﻿
+/* Index Navigation Buttons
+function doHome(){
+	$('html,body').animate({ scrollTop: $('html,body').offset().top}, 1500, 'easeOutExpo');
+};
+function doHealth(){
+	$('html,body').animate({ scrollTop: $('#row1').offset().top - 80}, 1500, 'easeOutExpo');
+};
+function doTime(){
+	$('html,body').animate({ scrollTop: $('#row2').offset().top - 80}, 1500, 'easeOutExpo');
+};
+function doMoney(){
+	$('html,body').animate({ scrollTop: $('#row3').offset().top - 80}, 1500, 'easeOutExpo');
+};
+function doContact(){
+	$('html,body').animate({ scrollTop: $('#row4').offset().top - 80}, 1500, 'easeOutExpo');
+};*/
+/*End Navigation Buttons*/
+
+function doStore(){
+	window.location.href='/store/index'; //go to store/index
+	$("#side").css("display", "block");
+	$("#clock").css("display", "block");
+};
+
+$(document).ready(function(){
+
+	
+});
--- app/assets/javascripts/navigate.js
+++ app/assets/javascripts/navigate.js
@@ -2,22 +2,6 @@
 var prodvarbrand;
 var qtySelected = 1;
 
-//Index Navigation Buttons
-function doHome(){
-	$('html,body').animate({ scrollTop: $('html,body').offset().top}, 1500, 'easeOutExpo');
-};
-function doHealth(){
-	$('html,body').animate({ scrollTop: $('#row1').offset().top - 80}, 1500, 'easeOutExpo');
-};
-function doTime(){
-	$('html,body').animate({ scrollTop: $('#row2').offset().top - 80}, 1500, 'easeOutExpo');
-};
-function doMoney(){
-	$('html,body').animate({ scrollTop: $('#row3').offset().top - 80}, 1500, 'easeOutExpo');
-};
-function doContact(){
-	$('html,body').animate({ scrollTop: $('#row4').offset().top - 80}, 1500, 'easeOutExpo');
-};
 /*End Navigation Buttons*/
 function doStore(){
 	window.location.href='/store/index'; //go to store/index
